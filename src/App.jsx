c8e47566e11bb6a15752397509c03a2e21b15065
Juan Castaneda <jccastanedacamargo@outlook.com>
2025-07-09 16:33:37 +0200

    feat: add new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,36 @@
+import { useEffect, useState } from 'react';
+import Loading from './Loading';
+
 const url = 'https://www.course-api.com/react-tours-project';
 
 const App = () => {
+  const [tours, setTours] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchTours = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setTours(tours);
+    } catch (error) {
+      console.log(error);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
   return <h2>Tours Starter</h2>;
 };
 export default App;
